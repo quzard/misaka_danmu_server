39912c140cbfb5df7796e082e4ecc207e8c70210
l429609201 <429609201@qq.com>
2025-08-19 18:10:41 +0800

    modified:   .github/workflows/docker-image.yml 	modified:   web/vite.config.js

--- web/vite.config.js
+++ web/vite.config.js
@@ -14,15 +14,6 @@ export default defineConfig({
       },
     },
   },
-  build: {
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
