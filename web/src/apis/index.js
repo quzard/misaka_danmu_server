dd708c955e29d8272e8cbba1e3eb2798e7df9d4a
balge <wchengba@gmail.com>
2025-08-19 16:41:40 +0800

    add stop task

--- web/src/apis/index.js
+++ web/src/apis/index.js
@@ -117,6 +117,8 @@ export const resumeTask = data =>
   api.post(`/api/ui/tasks/${data.taskId}/resume`, data)
 /** 删除任务 */
 export const deleteTask = data => api.delete(`/api/ui/tasks/${data.taskId}`)
+/** 中止任务 */
+export const stopTask = data => api.post(`/api/ui/tasks/${data.taskId}/abort`)
 /** 定时任务列表 */
 export const getScheduledTaskList = data =>
   api.get('/api/ui/scheduled-tasks', data)
