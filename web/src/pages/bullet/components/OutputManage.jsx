3dfa5a204d818236fb3b70d3442a4c1ed1d01022
balge <wchengba@gmail.com>
2025-09-11 17:28:11 +0800

    优化界面展示

--- web/src/pages/bullet/components/OutputManage.jsx
+++ web/src/pages/bullet/components/OutputManage.jsx
@@ -11,7 +11,7 @@ import { useMessage } from '../../../MessageContext'
 export const OutputManage = () => {
   const [loading, setLoading] = useState(false)
   const [limit, setLimit] = useState('-1')
-  const [enable, setEnable] = useState(false)
+  // const [enable, setEnable] = useState(false)
   const [saveLoading, setSaveLoading] = useState(false)
 
   const messageApi = useMessage()
@@ -19,12 +19,13 @@ export const OutputManage = () => {
   const getConfig = async () => {
     setLoading(true)
     try {
-      const [limitRes, enableRes] = await Promise.all([
-        getDanmuOutputTotal(),
-        getDanmuOutputAggregation(),
-      ])
+      // const [limitRes, enableRes] = await Promise.all([
+      //   getDanmuOutputTotal(),
+      //   getDanmuOutputAggregation(),
+      // ])
+      const limitRes = await getDanmuOutputTotal()
       setLimit(limitRes.data?.value ?? '-1')
-      setEnable(enableRes.data?.value === 'true' ? true : false)
+      // setEnable(enableRes.data?.value === 'true' ? true : false)
     } catch (e) {
       console.log(e)
       messageApi.error('获取配置失败')
@@ -36,10 +37,11 @@ export const OutputManage = () => {
   const handleSave = async () => {
     setSaveLoading(true)
     try {
-      await Promise.all([
-        setDanmuOutputTotal({ value: `${limit}` }),
-        setDanmuOutputAggregation({ value: enable ? 'true' : 'false' }),
-      ])
+      // await Promise.all([
+      //   setDanmuOutputTotal({ value: `${limit}` }),
+      //   setDanmuOutputAggregation({ value: enable ? 'true' : 'false' }),
+      // ])
+      await setDanmuOutputTotal({ value: `${limit}` })
       messageApi.success('保存成功')
     } catch (e) {
       messageApi.error('保存失败')
@@ -67,7 +69,7 @@ export const OutputManage = () => {
             3000-5000）。弹幕将从视频时长中均匀采样。
           </div>
         </div>
-        <div className="my-4">
+        {/* <div className="my-4">
           <div className="flex items-center justify-start gap-4 mb-2">
             <div>启用弹幕聚合</div>
             <Switch checked={enable} onChange={v => setEnable(v)} />
@@ -75,7 +77,7 @@ export const OutputManage = () => {
           <div>
             开启后，当播放器请求关联弹幕时（withRelated=true），系统将聚合所有源的弹幕。关闭则只返回当前源的弹幕。
           </div>
-        </div>
+        </div> */}
         <div className="flex items-center justify-end">
           <Button
             type="primary"
