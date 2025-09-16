351cb80f83ebbae1977ebe9120c38a5fe88ef161
l429609201 <429609201@qq.com>
2025-09-17 02:40:38 +0800

    modified:   src/scrapers/base.py

--- src/scrapers/base.py
+++ src/scrapers/base.py
@@ -3,15 +3,16 @@ import time
 import asyncio
 import re
 from abc import ABC, abstractmethod
-from typing import Any, Callable, Dict, List, Optional, Type
+from typing import Any, Callable, Dict, List, Optional, Type, TYPE_CHECKING
 from typing import Union
 import httpx
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
 from .. import crud
 from .. import models
-from ..config_manager import ConfigManager
 
+if TYPE_CHECKING:
+    from ..config_manager import ConfigManager
 
 def _roman_to_int(s: str) -> int:
     """将罗马数字字符串转换为整数。"""
@@ -92,7 +93,7 @@ class BaseScraper(ABC):
     _GLOBAL_EPISODE_BLACKLIST_DEFAULT = r"^(.*?)((.+?版)|(特(别|典))|((导|演)员|嘉宾|角色)访谈|福利|彩蛋|花絮|预告|特辑|专访|访谈|幕后|周边|资讯|看点|速看|回顾|盘点|合集|PV|MV|CM|OST|ED|OP|BD|特典|SP|NCOP|NCED|MENU|Web-DL|rip|x264|x265|aac|flac)(.*?)$"
     _PROVIDER_SPECIFIC_BLACKLIST_DEFAULT: str = ""
 
-    def __init__(self, session_factory: async_sessionmaker[AsyncSession], config_manager: ConfigManager):
+    def __init__(self, session_factory: async_sessionmaker[AsyncSession], config_manager: "ConfigManager"):
         self._session_factory = session_factory
         self.config_manager = config_manager
         self.logger = logging.getLogger(self.__class__.__name__)
