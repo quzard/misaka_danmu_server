2ded051806af52fcbad3679509bdcf1e0ab1f2bc
balge <wchengba@gmail.com>
2025-09-20 12:35:03 +0800

    优化ui

--- web/src/pages/control/components/ApiDoc.jsx
+++ web/src/pages/control/components/ApiDoc.jsx
@@ -20,7 +20,7 @@ export const ApiDoc = () => {
             className="w-full"
             style={{
               height: `calc(100vh - 300px)`,
-              backgroundColor: 'white',
+              backgroundColor: '#fff9fb',
             }}
             src="/api/control/docs"
           ></iframe>
