e91ced22483201745467f7b9021fc28dfe742810
l429609201 <429609201@qq.com>
2025-11-08 19:37:30 +0800

    modified:   web/src/pages/media-fetch/components/LocalItemList.jsx

--- web/src/pages/media-fetch/components/LocalItemList.jsx
+++ web/src/pages/media-fetch/components/LocalItemList.jsx
@@ -817,6 +817,31 @@ const LocalItemList = ({ refreshTrigger }) => {
         }
         extra={
           <Space>
+            <Button
+              icon={<TableOutlined />}
+              type={viewMode === 'table' ? 'primary' : 'default'}
+              onClick={() => setViewMode('table')}
+              size="small"
+            >
+              表格
+            </Button>
+            <Button
+              icon={<AppstoreOutlined />}
+              type={viewMode === 'card' ? 'primary' : 'default'}
+              onClick={() => setViewMode('card')}
+              size="small"
+            >
+              卡片
+            </Button>
+            <Segmented
+              value={mediaTypeFilter}
+              onChange={setMediaTypeFilter}
+              options={[
+                { label: '全部', value: 'all' },
+                { label: '电影', value: 'movie', icon: <VideoCameraOutlined /> },
+                { label: '电视节目', value: 'tv_series', icon: <PlaySquareOutlined /> },
+              ]}
+            />
             <Popconfirm
               title={`确定要删除选中的 ${selectedRowKeys.length} 个项目吗?`}
               onConfirm={handleBatchDelete}
@@ -869,23 +894,12 @@ const LocalItemList = ({ refreshTrigger }) => {
           >
             全选 ({selectedRowKeys.length}/{items.length})
           </Checkbox>
-          <Space>
-            <Search
-              placeholder="搜索标题"
-              allowClear
-              style={{ width: 200 }}
-              onSearch={setSearchText}
-            />
-            <Segmented
-              value={mediaTypeFilter}
-              onChange={setMediaTypeFilter}
-              options={[
-                { label: '全部', value: 'all' },
-                { label: '电影', value: 'movie', icon: <VideoCameraOutlined /> },
-                { label: '电视节目', value: 'tv_series', icon: <PlaySquareOutlined /> },
-              ]}
-            />
-          </Space>
+          <Search
+            placeholder="搜索标题"
+            allowClear
+            style={{ width: 200 }}
+            onSearch={setSearchText}
+          />
         </div>
 
         {viewMode === 'table' ? (
